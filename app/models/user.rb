02037f0bf3b73a2b27e8a860081150fa7d4cf41d
hamatoru <hamatoto3@gmail.com>
2023-09-29 13:23:26 +0900

    ユーザー管理機能周辺　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+          validates :nickname, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/, message: 'は全角文字で入力してください' }
+          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は文字と数字の両方を含めてください' }
+          end
 end
